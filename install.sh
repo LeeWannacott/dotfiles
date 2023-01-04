d9655109b9c89678a272c8976ab326671d6b934b
LeeWannacott <WannacottL@gmail.com>
2023-01-04 22:34:30 +1300

    Uncommented out libssl

--- install.sh
+++ install.sh
@@ -9,12 +9,11 @@ sudo apt-get install -y neovim
 sudo cp .vimrc ~/.vimrc
 mkdir -p ~/.config/nvim && \
 sudo cp .config/nvim/init.vim ~/.config/nvim/init.vim & \
-sudo cp .config/nvim/CocConfig.json ~/.config/nvim/CocConfig.json
+sudo cp .config/nvim/CocConfig.json ~/.config/nvim/coc-settings.json
 
 # install vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 
 # install gnome tweaks for rebinding caps lock
 sudo apt update
